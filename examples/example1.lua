dc74e9f2af4f3a6a0779c454c16d0afaacdda1fc
Armando Arce <armando.arce@gmail.com>
2018-08-18 12:16:51 -0600

    Example1 added

---
+++ examples/example1.lua
@@ -0,0 +1,10 @@
+require "pickol"  
+pic.ellipse("document")  
+pic.arrow()  
+pic.box("PIC")  
+pic.arrow()  
+pic.box("TBL/EQN","(optional)",dashed)  
+pic.arrow()  
+pic.box("TROFF")  
+pic.arrow()  
+pic.ellipse("typesetter")
