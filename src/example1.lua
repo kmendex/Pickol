d016b78dbd1a1d6f3dc4cee24c40dbe0fa805a87
Kenneth Méndez <kmendez@ic-itcr.ac.cr>
2018-08-21 12:02:02 -0600

    Implementadas primitivas basicas de dibujo

--- src/example1.lua
+++ src/example1.lua
@@ -1,11 +1,12 @@
 require "pickol"
 
-pic.ellipse("document")  
-pic.arrow()  
-pic.box("PIC")  
-pic.arrow()  
+pic.ellipse("document")
+pic.arrow()
+pic.box("PIC")
+pic.arrow()
 pic.box("TBL/EQN","(optional)",dashed)  
 pic.arrow()  
-pic.box("TROFF")  
-pic.arrow()  
+pic.box("TROFF")
+pic.arrow()
 pic.ellipse("typesetter")
+pic.PE()
--- src/pickol.lua
+++ src/pickol.lua
@@ -1,11 +1,56 @@
+local indX,indY=0,0
+local defaultSz=60
+local f = createFont("Vera.ttf",12)
+textFont(f)
+
 pic = pic or {}
 
-pic.arrow = function()
+pic.box = function(txt,dashed)
+	rect(indX,indY+15,50,30)
+	fill(0)
+	text(txt,indX+1,indY+25)
+	noFill()
+	indX=indX+50
 end
 
-pic.box = function(txt,option)
+pic.circle = function(txt)
+	
 end
 
 pic.ellipse = function(txt)
+	ellipse(indX+25,indY+25,50,30)
+	fill(0)
+	text(txt,indX+1,indY+25)
+	noFill()
+	indX=indX+50
 end
 
+pic.arc = function()
+	
+end
+
+pic.line = function()
+	
+end
+
+pic.arrow = function()
+	line(indX,indY+25,indX+50,indY+25)
+	indX=indX+50	
+end
+
+pic.spline = function()
+	
+end
+
+pic.move = function()
+	
+end
+
+pic.text_list = function()
+	
+end
+
+pic.PE = function()
+	indX,indY=0,0
+	noFill()
+end
